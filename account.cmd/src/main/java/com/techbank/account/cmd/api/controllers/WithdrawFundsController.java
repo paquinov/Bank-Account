6419641e353b2aab101ef932ac537b8645965dce
Paul Aquino Vega <paquinov@ext.falabella.cl>
2022-12-17 17:30:53 -0500

    feat: withdraw funds controller was added

---
+++ account.cmd/src/main/java/com/techbank/account/cmd/api/controllers/WithdrawFundsController.java
@@ -0,0 +1,43 @@
+package com.techbank.account.cmd.api.controllers;
+
+import com.techbank.account.cmd.api.commands.WithdrawFundsCommand;
+import com.techbank.account.common.dto.BaseResponse;
+import com.techbank.cqrs.core.exceptions.AggregateNotFoundException;
+import com.techbank.cqrs.core.infrastructure.CommandDispatcher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.text.MessageFormat;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@RestController
+@RequestMapping("/api/v1/withdrawFunds")
+public class WithdrawFundsController {
+
+    @Autowired
+    private CommandDispatcher commandDispatcher;
+
+    private final Logger logger = Logger.getLogger(WithdrawFundsController.class.getName());
+
+    @PutMapping("/{id}")
+    public ResponseEntity<BaseResponse> withdrawFunds(@PathVariable("id") String id, @RequestBody WithdrawFundsCommand command) {
+        try {
+            command.setId(id);
+            commandDispatcher.send(command);
+            return ResponseEntity.ok(new BaseResponse("Withdraw funds request was successful !"));
+        } catch (IllegalStateException | AggregateNotFoundException e) {
+            logger.log(Level.WARNING, MessageFormat.format("Client made a bad request: {0}", e.toString()));
+            return ResponseEntity.badRequest().body(new BaseResponse(e.toString()));
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, MessageFormat.format("Error while processing request to withdraw funds to bank account with id: {0}", id));
+            return ResponseEntity.internalServerError().body(new BaseResponse(e.toString()));
+        }
+    }
+
+}
