f7ee580ad03a3b053cf8f389ac28c1009d36e333
Paul Aquino Vega <paquinov@ext.falabella.cl>
2022-12-17 19:19:10 -0500

    fix: missing constructor added for close account event

--- account.common/src/main/java/com/techbank/account/common/events/AccountClosedEvent.java
+++ account.common/src/main/java/com/techbank/account/common/events/AccountClosedEvent.java
@@ -3,9 +3,11 @@ package com.techbank.account.common.events;
 
 import com.techbank.cqrs.core.events.BaseEvent;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @Data
+@NoArgsConstructor
 @SuperBuilder
 public class AccountClosedEvent extends BaseEvent {
 
